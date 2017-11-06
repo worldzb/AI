a71002b8dea67757affc462ba13e92a3f1206137
worldzb <yang641367741@gmail.com>
2017-11-06 18:57:54 +0800

    添加MySQL支持。--python3使用 pymysql，不支持mysqlDB

---
+++ AI/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: worldzb
+# @Date:   2017-10-30 23:45:53
+# @Last Modified by:   worldzb
+# @Last Modified time: 2017-11-06 18:52:34
+import pymysql;
+pymysql.install_as_MySQLdb();
--- AI/settings.py
+++ AI/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'AI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', #数据库引擎
+        'NAME': 'test',                       #数据库名
+        'USER': 'root',                       #用户名
+        'PASSWORD': '123456',                   #密码
+         'HOST': '',                           #数据库主机，默认为localhost
+        'PORT': '',                           #数据库端口，MySQL默认为3306
+        'OPTIONS': {
+            'autocommit': True,
+        },
     }
 }
 
